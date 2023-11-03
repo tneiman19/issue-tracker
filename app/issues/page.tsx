f5a70c0a9af999f5691bb2525cb0c0bf2b91fdbf
Tzvi (Steve) Neiman <tneiman19@gmail.com>
2023-11-02 20:14:42 -0400

    Show issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -8,12 +8,11 @@ import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000)
+  await delay(2000);
 
   return (
     <div>
-
-<IssueActions />
+      <IssueActions />
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
@@ -30,7 +29,7 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
