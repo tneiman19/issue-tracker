bd7ff90638da0d60a9dccb756aa45a6c9b15b256
Tzvi (Steve) Neiman <tneiman19@gmail.com>
2023-11-15 23:18:14 -0500

    Lay out the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Flex, Grid } from "@radix-ui/themes";
 import IssueChart from "./IssueChart";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
@@ -15,5 +16,13 @@ export default async function Home() {
     where: { status: "CLOSED" },
   });
 
-  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
 }
